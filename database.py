48c44b2aa89acbba0b6acd21abe6f40518d92b55
Vaishnavi Biradar <biradarvaishnavi444-png>
2026-06-30 10:49:01 +0530

    Refactor database connection and initialization functions for clarity and consistency

--- database.py
+++ database.py
@@ -1,28 +1,52 @@
 import sqlite3
-from flask import Flask,render_template,request,redirect,flash
-app = Flask(__name__)
-app.secret_key ='Linkiwi2026'   # Necessary for flash messages
+from flask import Flask, render_template, request, redirect, flash
 
-#2 function
-def get_db_():
-    """Database connection"""
+app = Flask(__name__)
+app.secret_key = 'Linkiwi2026'
+
+
+# Database Connection
+def get_db():
     conn = sqlite3.connect('Database.db')
-    conn.row_factory = sqlite3.Row  # To access columns by name
+    conn.row_factory = sqlite3.Row
     return conn
 
+
+# Create Tables
 def init_db():
-    """ Create table """
-    conn = get_db_()
-    # Create users table if it doesn't exist
+
+    conn = get_db()
+
+    # Visitors Table
     conn.execute("""
     CREATE TABLE IF NOT EXISTS visitors(
-         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         visitor_name TEXT NOT NULL,
         student_name TEXT NOT NULL,
         room_no TEXT NOT NULL,
         purpose TEXT NOT NULL
-   """ )
+    )
+    """)
+
+    # Users Table
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+    )
+    """)
+
+    try:
+        conn.execute("ALTER TABLE users ADD COLUMN role TEXT DEFAULT 'visitors'")
+    except:
+        pass
+
+    conn.commit()
     conn.close()
 
-    if __name__ == '__main__':
-        init_db()  # Initialize the database 
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
+    
