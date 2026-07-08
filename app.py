b5536c91a6e8f16cc9e0d216a799920a460a03c9
Vaishnavi Biradar <biradarvaishnavi444@gmail.com>
2026-07-08 14:17:05 +0530

    Restrict edit_visitor access to students only

--- app.py
+++ app.py
@@ -114,8 +114,8 @@ def delete_visitor(id):
 # =========================
 @app.route("/edit_visitor/<int:id>", methods=["GET", "POST"])
 def edit_visitor(id):
-    if session.get('role') !='admin':
-        flash("Admins only! You do not have permission","danger")
+    if session.get('role') !='student':
+        flash(" You do not have permission","danger")
         return redirect('/')
 
 
