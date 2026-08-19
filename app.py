57e63be2c3d831346b0ee5e5ce191e44d0c730b3
Vaishnavi Biradar <biradarvaishnavi444-png>
2026-08-19 15:41:11 +0530

    Refactor database connection to use dynamic path for Database.db

--- app.py
+++ app.py
@@ -30,12 +30,13 @@ def allowed_file(filename):
 # =========================
 # 1. DATABASE CONNECTION
 # =========================
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "Database.db")
+# Database Connection
 def get_db():
-    conn = sqlite3.connect("Database.db")
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
-
-
 # =========================
 # 2. INIT DATABASE
 # =========================
